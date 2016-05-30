597b2082dfc3c226f6591226621dd5889bac1b2f
Ross Marshall <rossmarshallgithub@gmail.com>
2016-05-30 00:21:25 -0400

    Test Spring Project for Linux 2

--- spring-tutorial-linux-5/src/main/java/com/caveofprogramming/spring/test/App.java
+++ spring-tutorial-linux-5/src/main/java/com/caveofprogramming/spring/test/App.java
@@ -9,7 +9,7 @@ public class App {
 		
 		ApplicationContext context = new FileSystemXmlApplicationContext( "beans.xml" );
 		
-		// Person person = new Person(); -POJO way to do it.
+		// Person person = new Person(); -POJO way to do it. 
 		Person person = (Person)context.getBean("person");  // Sprint way to do it.
 		person.speak();
 	}
