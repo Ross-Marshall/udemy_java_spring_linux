fc6bddb025957a346b9ebeb0083a6dda32d42a30
Ross Marshall <rossmarshallgithub@gmail.com>
2016-06-08 21:24:01 -0400

    From Linux

---
+++ spring-tutorial-36/new-data.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO `offers` VALUES (10, 'Pete', 'pete@nowhereatall.com', 'Will write Java for your organization');
+INSERT INTO `offers` VALUES (20, 'Kate', 'kate@nowhereatall.com', 'Web design, very cheap');
+INSERT INTO `offers` VALUES (70, 'Mary', 'mary@nowhereatall.com', 'PHP coding');
+INSERT INTO `offers` VALUES (80, 'Rog', ,'something or other', 'rog@nowhereatall.com');
+INSERT INTO `offers` VALUES (88, 'Rog', 'something or other', 'rog@nowhereatall.com');
